aca59c4a66126ab28022ba82abb93184dcd1750f
dlegland <david.legland@inrae.fr>
2025-01-24 17:10:33 +0100

    DelimitedTableReader: introduce reading a Table from an InputStream

--- src/main/java/ijt/table/io/DelimitedTableReader.java
+++ src/main/java/ijt/table/io/DelimitedTableReader.java
@@ -3,15 +3,18 @@
  */
 package ijt.table.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 
 import ijt.table.CategoricalColumn;
-import ijt.table.Table;
 import ijt.table.NumericColumn;
+import ijt.table.Table;
 
 /**
  * @author dlegland
@@ -96,19 +99,11 @@ public class DelimitedTableReader implements TableReader
 		this.readRowNames = readRowNames;
 	}
 
-
-	// =============================================================
-	// implementation of the TableReader interface
-
-	/* (non-Javadoc)
-	 * @see net.sci.table.io.TableReader#readTable()
-	 */
-	@Override
-	public Table readTable(File file) throws IOException
-	{
+    public Table readTable(InputStream stream) throws IOException
+    {
         // meta data for table
-	    
-	    // number of columns
+        
+        // number of columns
         int nCols;
         
         // table data, indexed by columns first.
@@ -120,114 +115,106 @@ public class DelimitedTableReader implements TableReader
         // the names of the rows
         ArrayList<String> rowNames = new ArrayList<String>();
 
-        // Create text reader from file 
-		LineNumberReader reader;
-		try 
-		{
-			reader = new LineNumberReader(new FileReader(file));
-		} 
-		catch(IOException ex)
-		{
-			throw new RuntimeException("Could not open file: " + file, ex);
-		}
-		
-		// number of rows read
-		int nRows = 0;
+        // convert stream to text reader that keep line number
+        LineNumberReader reader = new LineNumberReader(new BufferedReader(new InputStreamReader(stream)));
+        
+        // number of rows read
+        int nRows = 0;
 
-		// eventually skip some lines
-		for (int r = 0; r < skipLines; r++)
-		{
-			reader.readLine(); 
-		}
-		
-		String delimiterRegexp = "[" + delimiters + "]+";
+        // eventually skip some lines
+        for (int r = 0; r < skipLines; r++)
+        {
+            reader.readLine(); 
+        }
+        
+        String delimiterRegexp = "[" + delimiters + "]+";
 
-		// parse header line
-		String firstLine = reader.readLine();
-		String[] tokens = firstLine.split(delimiterRegexp);
+        // parse header line
+        String firstLine = reader.readLine();
+        String[] tokens = firstLine.split(delimiterRegexp);
 
-		// parse first line to identify number of columns 
-		if (readHeader)
-		{
-			// first line is the header
-			nCols = tokens.length - 1;
-			colNames = new String[nCols];
-			for (int i = 0; i < nCols; i++)
-			{
-				colNames[i] = tokens[i + 1];
-			}
-		}
-		else
-		{
-			// first line is a data line
-			nRows++;
-			nCols = tokens.length;
-			colNames = new String[nCols];
-		}
-		
-		// Allocate array lists for columns
-		columns = new ArrayList<ArrayList<String>>(nCols);
-		for (int c = 0; c < nCols; c++)
-		{
-			columns.add(new ArrayList<String>());
-		}
-		
-		if (!readHeader)
-		{
-			// read column values as strings
-			int offset = readRowNames ? 1 : 0;
-			for (int c = 0; c < nCols; c++)
-			{
-				columns.get(c).add(tokens[c + offset]);
-			}
-		}
-		
-		// read regular lines containing data
-		while (true)
-		{
-			String line = reader.readLine(); 
-			if (line == null || line.isEmpty())
-			{
-				break;
-			}
-			nRows++;
+        // parse first line to identify number of columns 
+        if (readHeader)
+        {
+            // first line is the header
+            nCols = tokens.length - 1;
+            colNames = new String[nCols];
+            for (int i = 0; i < nCols; i++)
+            {
+                colNames[i] = tokens[i + 1];
+            }
+        }
+        else
+        {
+            // first line is a data line
+            nRows++;
+            nCols = tokens.length;
+            colNames = new String[nCols];
+        }
+        
+        // Allocate array lists for columns
+        columns = new ArrayList<ArrayList<String>>(nCols);
+        for (int c = 0; c < nCols; c++)
+        {
+            columns.add(new ArrayList<String>());
+        }
+        
+        if (!readHeader)
+        {
+            // read column values as strings
+            int offset = readRowNames ? 1 : 0;
+            for (int c = 0; c < nCols; c++)
+            {
+                columns.get(c).add(tokens[c + offset]);
+            }
+        }
+        
+        // read regular lines containing data
+        while (true)
+        {
+            String line = reader.readLine(); 
+            if (line == null || line.isEmpty())
+            {
+                break;
+            }
+            nRows++;
 
-			// split the tokens of current line
-			tokens = line.split(delimiterRegexp);
-			
-			// read row name
-			if (readRowNames)
-			{
-				rowNames.add(tokens[0]);
-				// read column values as strings
-				for (int c = 0; c < nCols; c++)
-				{
-					columns.get(c).add(tokens[c + 1]);
-				}			
-			}
-			else
-			{
-				// read column values as strings
-				for (int c = 0; c < nCols; c++)
-				{
-					columns.get(c).add(tokens[c]);
-				}			
-			}
-		}
-		
-		reader.close();
-		
-		// convert columns
+            // split the tokens of current line
+            tokens = line.split(delimiterRegexp);
+            
+            // read row name
+            if (readRowNames)
+            {
+                rowNames.add(tokens[0]);
+                // read column values as strings
+                for (int c = 0; c < nCols; c++)
+                {
+                    columns.get(c).add(tokens[c + 1]);
+                }           
+            }
+            else
+            {
+                // read column values as strings
+                for (int c = 0; c < nCols; c++)
+                {
+                    columns.get(c).add(tokens[c]);
+                }           
+            }
+        }
+        
+        reader.close();
+        
+        // convert columns
         Table table = Table.create(nRows, nCols);
-		
-		// convert string arrays to double values
-		for (int c = 0; c < nCols; c++)
-		{
-		    // tokens of current column
-			ArrayList<String> colData = columns.get(c);
-			
-			// check if column contains only numeric values
-			double[] values = new double[nRows];
+        
+        // convert string arrays to double values
+        for (int c = 0; c < nCols; c++)
+        {
+            // tokens of current column
+            ArrayList<String> colData = columns.get(c);
+            
+            // check if column contains only numeric values
+            double[] values = new double[nRows];
             boolean isNumeric = true;
             for (int r = 0; r < nRows; r++)
             {
@@ -251,16 +238,36 @@ public class DelimitedTableReader implements TableReader
             {
                 table.setColumn(c, CategoricalColumn.create(colData.toArray(new String[0])));
             }
-		}
+        }
 
-		// populates meta-data
-		table.setColumnNames(colNames);
-		if (readRowNames)
-			table.setRowNames(rowNames.toArray(new String[0]));
-		
-		// also set the name of the table to the name of the file
-		table.setName(file.getName());
-		
-		return table;
-	}
+        // populates meta-data
+        table.setColumnNames(colNames);
+        if (readRowNames)
+        {
+            table.setRowNames(rowNames.toArray(new String[0]));
+        }
+        
+        return table;
+    }
+    
+    
+    // =============================================================
+    // implementation of the TableReader interface
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sci.table.io.TableReader#readTable()
+     */
+    @Override
+    public Table readTable(File file) throws IOException
+    {
+        // create input stream from file, and call the delegated method.
+        Table table = readTable(new FileInputStream(file));
+
+        // set the name of the table to the name of the file
+        table.setName(file.getName());
+
+        return table;
+    }
 }
